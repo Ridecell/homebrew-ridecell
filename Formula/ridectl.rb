7bb298ba071c8c5b36a3de07bff3b38e6e04c742
Noah Kantrowitz <noah@coderanger.net>
2019-03-11 18:11:25 -0700

    Update ridectl.rb

--- Formula/ridectl.rb
+++ Formula/ridectl.rb
@@ -1,8 +1,8 @@
 class Ridectl < Formula
   desc "Command line tool for ridecell-operator"
   homepage "https://github.com/Ridecell/ridectl"
-  url "https://github.com/Ridecell/ridectl/releases/download/v0.0.9/ridectl_macos.zip"
-  sha256 "d5fb878dbcec2e0147f6f0e681cfad10268f0b8d05ee9522d36b520d2c9153ec"
+  url "https://github.com/Ridecell/ridectl/releases/download/v0.0.10/ridectl_macos.zip"
+  sha256 "6704587a0326663c2be90723c09a9e904c6fee4b491d3a0e3fb66046d44231c8"
 
   def install
     bin.install "ridectl"
