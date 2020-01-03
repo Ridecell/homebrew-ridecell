6b257d393da0a7d6ed013c5338f6190ab94768cc
Marina Ma <marina@ridecell.com>
2020-01-03 14:35:56 -0800

    satisfy sha256sum

--- Formula/ridectl.rb
+++ Formula/ridectl.rb
@@ -1,11 +1,14 @@
 class Ridectl < Formula
-  # extract release tag from redirect link
+  # extract release tag from redirect link and get shasum
   regex = %r{/tag/([^"]+)}
   @latest_tag = `curl -s https://github.com/Ridecell/ridectl/releases/latest`.match(regex)[1]
+  shasum = `curl -sfL "https://github.com/Ridecell/ridectl/releases/download/#{@latest_tag}/ridectl_macos.zip" | shasum -a 256 | awk '{print $1}'`
+
   desc "Command-line tool for ridecell-operator"
   homepage "https://github.com/Ridecell/ridectl"
   url "https://github.com/Ridecell/ridectl/releases/download/#{@latest_tag}/ridectl_macos.zip"
   version @latest_tag.delete_prefix("v")
+  sha256 shasum.strip
 
   def install
     bin.install "ridectl"
