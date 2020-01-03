1765328b338761200d5253b760cea814722577b5
Marina Ma <marina@ridecell.com>
2020-01-03 12:21:38 -0800

    make brew audit happy

--- Formula/ridectl.rb
+++ Formula/ridectl.rb
@@ -1,10 +1,11 @@
 class Ridectl < Formula
   # extract release tag from redirect link
-  @latest_tag = `curl -s https://github.com/Ridecell/ridectl/releases/latest`[/tag\/([^"]+)/,1]
-  desc "Command line tool for ridecell-operator"
+  regex = %r{/tag/([^"]+)}
+  @latest_tag = `curl -s https://github.com/Ridecell/ridectl/releases/latest`.match(regex)[1]
+  desc "Command-line tool for ridecell-operator"
   homepage "https://github.com/Ridecell/ridectl"
-  url "https://github.com/Ridecell/ridectl/releases/download/#@latest_tag/ridectl_macos.zip"
-  # sha256 "6949a13dddc537e60a7db990d97e985807ddf48be21732f65dd2a2554ed37d51"
+  url "https://github.com/Ridecell/ridectl/releases/download/#{@latest_tag}/ridectl_macos.zip"
+  version @latest_tag.delete_prefix("v")
 
   def install
     bin.install "ridectl"
